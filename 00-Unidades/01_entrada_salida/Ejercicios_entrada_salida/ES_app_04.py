ddfd39a3070765bb04dc8fff97ace3b43a6e0d03
Juan Manuel Fernandez Casenave <juan.manuel.fc@gmail.com>
2024-02-07 08:55:21 -0300

    finalizado el ejer 4 de entradas/salidas

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_04.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_04.py
@@ -20,11 +20,23 @@ class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
-        self.title("UTN FRA")
+        # self      el objeto que se está creando
+        # .title    el (atributo?) del objeto? es parte del objeto que se está creando
+        # ("UTN FRA 2024")  el argumento que se le está pasando al title
+        # el título de la ventana que aparece cuando se ejecuta "play" en el editor de código
+        self.title("UTN FRA 2024")
 
-        self.label1 = customtkinter.CTkLabel(master=self, text="Nombre")
+        # .label1
+        # la "etiqueta" que aparece en la ventana, como si fuese el <label> de HTML?
+        self.label1 = customtkinter.CTkLabel(master=self, text="Nombre y Apellido:")
+
+        # el tamaño del labal
+        # padx = padding horizontal a izquierda y derecha de la etiqueta
+        # pady = padding vertical arriba y abajo de la etiqueta
+        # self.label1.grid(row=0, column=0, padx=190, pady=230)
         self.label1.grid(row=0, column=0, padx=20, pady=10)
-        
+
+        # txt_nombre = el texto que se ingresa en la caja de texto 
         self.txt_nombre = customtkinter.CTkEntry(master=self)
         self.txt_nombre.grid(row=0, column=1)
         
@@ -33,7 +45,14 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        # Obtengo el texto de self.txt_nombre
+        texto_ingresado = self.txt_nombre.get()
+        # calculo la cantidad de caracteres del texto ingresado
+        caracteres = len(texto_ingresado)
+        # los borro
+        self.txt_nombre.delete(0, caracteres)
+        # los ingreso nuevament
+        self.txt_nombre.insert(0, texto_ingresado)
         
     
 if __name__ == "__main__":
