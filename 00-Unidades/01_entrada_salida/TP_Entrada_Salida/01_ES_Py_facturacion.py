af4793f7bf51c24a3f7940ac2a19ba28ffbfd457
Juan Manuel Fernandez Casenave <juan.manuel.fc@gmail.com>
2024-02-08 09:14:48 -0300

    finalizado el TP01 de entradas/salidas

--- 00-Unidades/01_entrada_salida/TP_Entrada_Salida/01_ES_Py_facturacion.py
+++ 00-Unidades/01_entrada_salida/TP_Entrada_Salida/01_ES_Py_facturacion.py
@@ -52,14 +52,50 @@ class App(customtkinter.CTk):
         self.btn_total_iva.grid(row=5, pady=10, columnspan=2, sticky="nsew")
 
     def btn_total_on_click(self):
-        pass
+        # A. Ingresar tres precios de productos y mostrar la suma de los mismos.
+        # Entrada: obtener los precios de los tres productos
+        precio_1 = int(self.txt_importe_1.get())
+        precio_2 = int(self.txt_importe_2.get())
+        precio_3 = int(self.txt_importe_3.get())
+
+        # Proceso: sumar los tres precios
+        total = precio_1 + precio_2 + precio_3 
+
+        # Salida: mostrar la suma
+        alert("La suma de los tres productos", ("{0} + {1} + {2} = {3}".format(precio_1, precio_2, precio_3, total)))
 
     def btn_promedio_on_click(self):
-        pass
+        # B. Ingresar tres precios de productos y mostrar el promedio de los mismos.
+        # Entrada: obtener los precios de los tres productos
+        precio_1 = int(self.txt_importe_1.get())
+        precio_2 = int(self.txt_importe_2.get())
+        precio_3 = int(self.txt_importe_3.get())
+        lista_precios = [precio_1, precio_2, precio_3]
+
+        # Proceso: calcular el promedio de los tres precios
+        lista_precios = [precio_1, precio_2, precio_3]
+        promedio = int(sum(lista_precios) / len(lista_precios))
+
+        # Salida: mostrar la suma
+        alert("El promedio de los tres productos", ("El promedio = {0}".format(promedio)))
+
 
     def btn_total_iva_on_click(self):
-        pass      
-    
+        # C	ingresar tres precios de productos sumarlos y mostrar el precio final (más IVA 21%).
+
+        # Entrada: obtener los precios de los tres productos
+        precio_1 = int(self.txt_importe_1.get())
+        precio_2 = int(self.txt_importe_2.get())
+        precio_3 = int(self.txt_importe_3.get())
+
+        # Proceso: sumarlos y agregarle IVA
+        lista_precios = [precio_1, precio_2, precio_3]
+        iva = int(sum(lista_precios) * 0.21)
+        precios_mas_iva = int(sum(lista_precios) * 1.21) 
+
+        # Salida: mostrar el precio final mas IVA
+        alert("El precio final + IVA", "${0} + ${1} + ${2} + ${3}(IVA) = ${4}".format(precio_1, precio_2, precio_3, iva, precios_mas_iva))
+
 if __name__ == "__main__":
     app = App()
     app.geometry("300x300")
