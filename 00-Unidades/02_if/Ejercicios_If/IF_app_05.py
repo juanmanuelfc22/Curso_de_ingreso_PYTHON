ced6629ea8d39e73992883298895d61c2224e155
Juan Manuel Fernandez Casenave <juan.manuel.fc@gmail.com>
2024-02-12 06:13:57 -0300

    ejercicios num 5 de IF finalizado

--- 00-Unidades/02_if/Ejercicios_If/IF_app_05.py
+++ 00-Unidades/02_if/Ejercicios_If/IF_app_05.py
@@ -13,6 +13,8 @@ Ejercicio: if_05
 Enunciado:
 Al presionar el botón 'Calcular', se deberá obtener el contenido de la caja de texto txtEdad, 
 transformarlo en número e informar si la persona "NO ES ADOLESCENTE" utilizando el Dialog Alert.
+Enunciado del ejercicio anterior:
+edad entre 13 y 17 años ES ADOLESCENTE. Se deberá informar utilizando el Dialog alert.
 '''
 
 class App(customtkinter.CTk):
@@ -28,16 +30,23 @@ class App(customtkinter.CTk):
         self.txt_edad = customtkinter.CTkEntry(master=self)
         self.txt_edad.grid(row=0, column=1)
                 
-        self.btn_mostrar = customtkinter.CTkButton(master=self, text="Mostrar", command=self.btn_mostrar_on_click)
+        self.btn_mostrar = customtkinter.CTkButton(master=self, text="Calcular", command=self.btn_mostrar_on_click)
         self.btn_mostrar.grid(row=2, pady=20, columnspan=2, sticky="nsew")
 
 
     def btn_mostrar_on_click(self):
-        pass
+        # Entrada
+        # E.1 Obtener la edad de la persona
+        # E.2 Convertirlo a número
+        edad = self.txt_edad.get()
+        edad = int(edad)
 
-        
-        
-    
+        # Proceso
+        # P.1 Calcular si es adolescnete 13 y 17 años ES ADOLESCENTE
+        if edad >=13 and edad<=17:
+            alert("Ejerci num 5 de IF","ES Adolescente")
+
+
 if __name__ == "__main__":
     app = App()
     app.geometry("300x300")
