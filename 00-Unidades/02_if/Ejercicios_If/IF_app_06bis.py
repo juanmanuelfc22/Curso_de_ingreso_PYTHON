efdc33bfcb210c6eca95e1e4e7e933618b58c16a
Juan Manuel Fernandez Casenave <juan.manuel.fc@gmail.com>
2024-02-12 09:08:21 -0300

    finalizado ejer 6bis de IF

--- 00-Unidades/02_if/Ejercicios_If/IF_app_06bis.py
+++ 00-Unidades/02_if/Ejercicios_If/IF_app_06bis.py
@@ -34,16 +34,39 @@ class App(customtkinter.CTk):
         self.txt_altura = customtkinter.CTkEntry(master=self)
         self.txt_altura.grid(row=0, column=1)
                 
-        self.btn_mostrar = customtkinter.CTkButton(master=self, text="Mostrar", command=self.btn_mostrar_on_click)
+        self.btn_mostrar = customtkinter.CTkButton(master=self, text="Determinar", command=self.btn_mostrar_on_click)
         self.btn_mostrar.grid(row=2, pady=20, padx=30, columnspan=2, sticky="nsew")
 
 
     def btn_mostrar_on_click(self):
-        pass
+        # Menos de 160 cm: Base
+        # Entre 160 cm y 179 cm: Escolta
+        # Entre 180 cm y 199 cm: Alero
+        # 200 cm o más: Ala-Pívot o Pívot
+        # Entrada
+        # E1. obtener la altura 
+        # E2. convertir a numero
+        altura = self.txt_altura.get()
+        altura = int(altura)
 
-        
-        
-    
+        # Proceso y Salida
+        # Comparar la altura con las pautas y efectular la salida
+        titulo_ejer = "Ejercicio 6bis IF"
+        es_base = "Es Base"
+        es_escolta = "Es Escolta"
+        es_alero = "Es Alero"
+        es_pivot = "Es Ala-Pivot o Pivot"
+
+        if altura < 160:
+            alert(titulo_ejer, es_base)
+        elif altura >= 160 and altura <= 179:
+            alert(titulo_ejer, es_escolta)
+        elif altura >= 180 and altura <= 199:
+            alert(titulo_ejer, es_alero)
+        else:
+            alert(titulo_ejer, es_pivot)
+
+
 if __name__ == "__main__":
     app = App()
     app.geometry("300x300")
