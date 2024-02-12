7ae6eb36a2fb03f90377afd76b7ebdc8e0cd16b6
Juan Manuel Fernandez Casenave <juan.manuel.fc@gmail.com>
2024-02-12 06:02:20 -0300

    modificado el ejer 3bis de IF

--- 00-Unidades/02_if/Ejercicios_If/IF_app_04.py
+++ 00-Unidades/02_if/Ejercicios_If/IF_app_04.py
@@ -30,7 +30,7 @@ class App(customtkinter.CTk):
         self.txt_edad.grid(row=0, column=1)
 
         self.btn_mostrar = customtkinter.CTkButton(
-            master=self, text="Mostrar", command=self.btn_mostrar_on_click)
+            master=self, text="Calcular", command=self.btn_mostrar_on_click)
         self.btn_mostrar.grid(row=2, pady=20, columnspan=2, sticky="nsew")
 
     def btn_mostrar_on_click(self):
