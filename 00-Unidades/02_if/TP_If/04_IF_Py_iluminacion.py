e8c9cb864294a5207d346cc4d08ae5ee22b78ac2
Juan Manuel Fernandez <juan.manuel.fc@gmail.com>
2024-02-13 19:43:49 -0300

    TP if comenzado

--- 00-Unidades/02_if/TP_If/04_IF_Py_iluminacion.py
+++ 00-Unidades/02_if/TP_If/04_IF_Py_iluminacion.py
@@ -11,12 +11,22 @@ apellido:
 TP: IF_Iluminacion
 ---
 Enunciado:
-Todas las lámparas están  al mismo precio de $800 pesos final.
-		A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
-		B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
-		C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
-		D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
-		E.	Si el importe final con descuento suma más de $4000  se obtien un descuento adicional de 5%.
+Todas las lámparas están al mismo precio de $800 pesos final.
+
+		A.	Si compra 6 o más lamparitas bajo consumo tiene un descuento del 50%. 
+
+		B.	Si compra 5 lamparitas bajo consumo marca "ArgentinaLuz"
+        => se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
+
+		C. Si compra 4 lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas”
+        => se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
+
+		D.	Si compra 3 lamparitas bajo consumo marca "ArgentinaLuz"
+        => el descuento es del 15%,
+        => si es “FelipeLamparas” se hace un descuento del 10 % 
+        => y si es de otra marca un 5%.
+
+		E.	Si el importe final con descuento suma más de $4000 se obtien un descuento adicional de 5%.
 '''
 
 class App(customtkinter.CTk):
@@ -43,7 +53,78 @@ class App(customtkinter.CTk):
 
 
     def btn_calcular_on_click(self):
-        pass
+
+        # Entrada
+        # E1. Obtener la cantidad de lamparitas
+        # E2. Obtener la marca de las lamparitas
+        cantidad = int(self.combobox_cantidad.get())
+        marca = self.combobox_marca.get()
+
+        # Proceso y salida
+        # En funcion de las reglas de negocio: según la cantidad y la marca de las lamparitas
+        # calculo el precio final y luego aplico los descuentos
+        precio = 800 * cantidad
+
+        # Si compra 6 o más lamparitas bajo consumo tiene un descuento del 50%. 
+        if cantidad >= 6:
+            desc = int(precio * 0.5)
+            desc_str = "Descuento -$" + str(desc) + "\n"
+            total = int(precio - desc)
+            tot_str = "Total $" + str(total) + "\n"
+
+            if total > 4000:
+                desc_adicional = int(total * .05)
+                desc_adic_str = "Descuento adic -$" + str(desc_adicional) + "\n"
+                total_final = precio - desc_adicional
+                total_final_str = "TOTAL FINAL $" + str(total_final)
+                alert("TP if", "${0}{1}{2}{3}{4}".format(total, desc_str, tot_str, desc_adic_str, total_final_str))
+
+            # alert("TP if", "Precio ${0}\n- Descuento ${1}\nTotal ${2}".format(precio, descuento, total))
+        # Si compra 5 lamparitas bajo consumo marca "ArgentinaLuz"
+        # se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
+        elif cantidad == 5:
+            if marca == "ArgentinaLuz":
+                descuento = int(precio * 0.4)
+                total = int(precio - descuento)
+                alert("TP if", "Precio ${0}\n- Descuento ${1}\nTotal ${2}".format(precio, descuento, total))
+            else:
+                descuento = int(precio * 0.3)
+                total = int(precio - descuento)
+                alert("TP if", "Precio ${0}\n- Descuento ${1}\nTotal ${2}".format(precio, descuento, total))
+        # Si compra 4 lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas”
+        # se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
+        elif cantidad == 4:
+            if marca == "ArgentinaLuz" or marca == "FelipeLamparas":
+                descuento = int(precio * 0.25)
+                total = int(precio - descuento)
+                alert("TP if", "Precio ${0}\n- Descuento ${1}\nTotal ${2}".format(precio, descuento, total))
+            else:
+                descuento = int(precio * 0.2)
+                total = int(precio - descuento)
+                alert("TP if", "Precio ${0}\n- Descuento ${1}\nTotal ${2}".format(precio, descuento, total))
+        # Si compra 3 lamparitas bajo consumo marca "ArgentinaLuz"
+        # el descuento es del 15%, si es “FelipeLamparas” se hace un descuento del 10 % 
+        # y si es de otra marca un 5%.
+        elif cantidad == 3:
+            if marca == "ArgentinaLuz":
+                descuento = int(precio * 0.15)
+                total = int(precio - descuento)
+                alert("TP if", "Precio ${0}\n- Descuento ${1}\nTotal ${2}".format(precio, descuento, total))
+            elif marca == "FelipeLamparas":
+                descuento = int(precio * 0.1)
+                total = int(precio - descuento)
+                alert("TP if", "Precio ${0}\n- Descuento ${1}\nTotal ${2}".format(precio, descuento, total))
+            else:
+                descuento = int(precio * 0.05)
+                total = int(precio - descuento)
+                alert("TP if", "Precio ${0}\n- Descuento ${1}\nTotal ${2}".format(precio, descuento, total))
+        else:
+            alert("TP if", "No se puedo calcular por haber comprado menos de 3 lamparitas")
+
+
+
+
+
         
     
 if __name__ == "__main__":
