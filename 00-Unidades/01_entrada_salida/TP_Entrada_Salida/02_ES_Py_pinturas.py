6622e3480788ec04fd4d79cccf4146619b5cb1f0
Juan Manuel Fernandez <juan.manuel.fc@gmail.com>
2024-02-13 14:56:49 -0300

    finalizado TP2 entradas/salidas

--- 00-Unidades/01_entrada_salida/TP_Entrada_Salida/02_ES_Py_pinturas.py
+++ 00-Unidades/01_entrada_salida/TP_Entrada_Salida/02_ES_Py_pinturas.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Juan Manuel
+apellido: Fernandez Casenave
 ---
 TP: ES_Pinturas
 ---
@@ -41,38 +41,49 @@ class App(customtkinter.CTk):
         self.txt_temperatura_f = customtkinter.CTkEntry(master=self)
         self.txt_temperatura_f.grid(row=1, column=1)
        
-        self.btn_convertir_c_f = customtkinter.CTkButton(master=self, text="Convertir °C a °F", command=self.btn_convertir_c_f_on_click)
+        self.btn_convertir_c_f = customtkinter.CTkButton(master=self, text="Convertir °F a °C", command=self.btn_convertir_c_f_on_click)
         self.btn_convertir_c_f.grid(row=3, pady=10, columnspan=2, sticky="nsew")
         
-        self.btn_convertir_f_c = customtkinter.CTkButton(master=self, text="Convertir °F a °C", command=self.btn_convertir_f_c_on_click)
+        self.btn_convertir_f_c = customtkinter.CTkButton(master=self, text="Convertir °C a °F", command=self.btn_convertir_f_c_on_click)
         self.btn_convertir_f_c.grid(row=4, pady=10, columnspan=2, sticky="nsew")
     
     def btn_convertir_c_f_on_click(self):
         # A. Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado 
         # (0 °F − 32) × 5/9 = -17,78 °C
 
-        # Entrada: obtener el dato a convertir, los °F para converitr a °C
-        temp_f = round(float(self.txt_temperatura_c.get()),2)
+        # Entrada.
+        # E1. Obtener la temperatura en °F
+        # E2. Convertir la temperatura de string a float con dos decimales
+        temperatura_f = self.txt_temperatura_f.get()
+        temperatura_f = round(float(temperatura_f), 2)
 
-        # Proceso: convertir la temp a °F
-        temp_f_a_c = round(((temp_c * 9 / 5) + 32),2)
+        # Proceso.
+        # P1. Convertir la temperatura de °F a °C con dos decimales
+        temperatura_c = round((temperatura_f - 32) * 5/9, 2)
 
-        # Salida: aplicar la leyenda correspondiente
-        alert("Conversor de temp de F° a C°", "({0}°C - 32) x 5/9 = {1}°F".format(temp_f, temp_f_a_c))
-
+        # Salida.
+        # S1. Mostrar la temperatura en °C
+        alert("TP num 2 entradas/salidas", "({0} °F - 32) * 5/9 = {1} °C".format(temperatura_f, temperatura_c))
 
 
     def btn_convertir_f_c_on_click(self):
         # B. Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit 
         # (0 °C × 9/5) + 32 = 32 °F
 
-        # Entrada: obtener los los °C para convertir a °F
-        temp_c = self.txt
+        # Entrada.
+        # E1. Obtener la temperatura en °C
+        # E2. Convertir la temperatura de string a float con dos decimales
+        temperatura_c = self.txt_temperatura_c.get()
+        temperatura_c = round(float(temperatura_c), 2)
 
+        # Proceso.
+        # P1. Convertir la temperatura de °C a °F con dos decimales
+        temperatura_f = round((temperatura_c * 9/5) + 32, 2)
 
-        temp_f = self.txt_temperatura_f.get()
-        print(temp_f)
-    
+        # Salida.
+        # S1. Mostrar la temperatura en °F
+        alert("TP num 2 entradas/salidas", "({0} °C × 9/5) + 32 = {1} °F".format(temperatura_c, temperatura_f))
+
     
 if __name__ == "__main__":
     app = App()
