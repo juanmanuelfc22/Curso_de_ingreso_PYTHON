f326989f876c35fd2eb1d45da8865629e797c07e
Juan Manuel Fernandez <juan.manuel.fc@gmail.com>
2024-02-18 12:54:23 -0300

    finalizado TP de MATCH

--- 00-Unidades/03_match/TP_Match/04_Match_Py_iluminacion.py
+++ 00-Unidades/03_match/TP_Match/04_Match_Py_iluminacion.py
@@ -43,7 +43,86 @@ class App(customtkinter.CTk):
 
 
     def btn_calcular_on_click(self):
-        pass
+    # Todas las lámparas están  al mismo precio de $800 pesos final.
+		# A. Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
+        #
+		# B. Si compra 5  lamparitas bajo consumo
+        # marca "ArgentinaLuz" se hace un descuento del 40 %
+        # si es de otra marca el descuento es del 30%.
+        #
+		# C. Si compra 4  lamparitas bajo consumo
+        # marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 %
+        # y si es de otra marca el descuento es del 20%.
+        #
+		# D. Si compra 3  lamparitas bajo consumo
+        # marca "ArgentinaLuz"  el descuento es del 15%,
+        # si es  “FelipeLamparas” se hace un descuento del 10 %
+        # y si es de otra marca un 5%.
+        # 
+		# E. Si el importe final con descuento suma más de $4000
+        # se obtien un descuento adicional de 5%.
+        cant_lamp = self.combobox_cantidad.get()
+        cant_lamp = int(cant_lamp)
+
+        marca = self.combobox_marca.get()
+
+        fc_inicial = cant_lamp * 800
+        descuento = 0
+
+        match cant_lamp:
+            case cant_lamp if cant_lamp >= 6:
+                descuento = 0.5
+            case 5:
+                match marca:
+                    case "ArgentinaLuz":
+                        descuento = 0.4
+                    case _:
+                        descuento = 0.3
+            case 4:
+                match marca:
+                    case "ArgentinaLuz" | "FelipeLamparas":
+                        descuento = 0.25
+                    case _:
+                        descuento = 0.2
+            case 3:
+                match marca:
+                    case "ArgentinaLuz":
+                        descuento = 0.15
+                    case "FelipeLamparas":
+                        descuento = 0.1
+                    case _:
+                        descuento = 0.05
+            case _:
+                descuento = 0
+
+        monto_desc = int(fc_inicial * descuento)
+        fc_total = fc_inicial - monto_desc
+        fc_final = fc_total
+
+
+        match fc_total:
+            case fc_total if fc_total > 4000:
+                desc_adicional = 0.05
+                monto_desc_adic = fc_total * desc_adicional
+                fc_final = fc_total - monto_desc_adic
+            case _:
+                monto_desc_adic = 0
+
+
+        informe_final = (
+            f"Compra de {cant_lamp} u. {marca}\n\n"
+            f"Total\t\t ${fc_inicial}\n"
+            f"Descuento {int((descuento * 100))}%\t-${monto_desc}\n"
+            f"Total parcial\t ${fc_total}\n"
+            f"---\n"
+            f"Desc +5% si parcial supera $4000\n"
+            f"Desc adic.\t-${monto_desc_adic}\n"
+            f"TOTAL final\t ${fc_final}"
+        )
+
+        alert("TP MATCH", informe_final)
+
+
         
     
 if __name__ == "__main__":
