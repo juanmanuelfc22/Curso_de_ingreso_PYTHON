c8cb92542ca1c276b20f56d25fd86079bfe61041
Juan Manuel Fernandez <juan.manuel.fc@gmail.com>
2024-02-24 07:36:24 -0300

    Agregados todos los ejerc de while modificados

---
+++ 00-Unidades/05_for/Ejercicios_For/clase-for.py
@@ -0,0 +1,27 @@
+# Iteraciones For con Python
+# Es otra forma de hacer iteraciones 
+# cual es la diferencia con el while?
+# con while se hace una iteración hasta que la condición sea falsa
+# con for se hace una iteración por cada elemento de una secuencia
+# con el for se hasta cirta cantidad de veces
+# con ejercicios que diga que se haga 10 veces, se usa el for
+# con ejercicios que diga que se haga hasta que el usuario quiera, se usa el while
+
+# el for necesita una variable local
+# generalmente se usa por defecto la variable i
+# se puede usar cualquier nombre de variable
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(0,100,10):
+#    print(i)
+
+# for i in range(100,0,-10):
+#    print(i)
+
+# for i in range(10,0,-1):
+#    print(i)
+
+for i in range(10,0,-2):
+    print(i)
