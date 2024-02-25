9086b78558fbe2ed6345c0cb660d15435e98b0af
Juan Manuel Fernandez <juan.manuel.fc@gmail.com>
2024-02-25 09:13:47 -0300

    modificado examen de prueba

--- 00-Unidades/04_while/Ejercicios_While/practica-examen.py
+++ 00-Unidades/04_while/Ejercicios_While/practica-examen.py
@@ -40,11 +40,9 @@ class App(customtkinter.CTk):
         cont_menor = 0
         cont_menor_y_fiebre = 0
         acumu_fiebre = 0
-        registro_temp_mas_baja_fem = False
         mombre_temp_mas_baja_fem = ""
-        temp_mas_baja_fem = 0
 
-        while cont_while <= 5:
+        while cont_while < 3:
 
             # Prompts y validaciones iniciales
             nombre = prompt("Nombre", "Ingresar nombre")
@@ -94,7 +92,6 @@ class App(customtkinter.CTk):
             #  así también la temp promedio de las personas menores de edad
             if temp >= 37:
                 cont_fiebre = cont_fiebre + 1
-                acumu_fiebre = acumu_fiebre + temp
 
             # contador de las personas menores de edad
             if edad < 18:
@@ -103,19 +100,20 @@ class App(customtkinter.CTk):
             # cont de personas menores que tieneen fiebre
             if edad < 18 and temp >= 37:
                 cont_menor_y_fiebre = cont_menor_y_fiebre + 1
+                acumu_fiebre = acumu_fiebre + temp
 
             # Si la persona es de sexo f y registra fiebre, guardar
             # su nombre y su temp registrada de fiebre
             # también cambiar el valor de la bandera si existe tal perosona
             # (mujer con fiebre)
+
             if (sexo == "f" and temp >= 37):
-                registro_temp_mas_baja_fem = True
                 mombre_temp_mas_baja_fem = nombre
                 temp_mas_baja_fem = temp
 
             # si bandera de la mujer de arriba existe, y si su temp es más
             # baja de la registrada, guardar los datos otra vez: nombre y temp
-            if (registro_temp_mas_baja_fem == True and temp < temp_mas_baja_fem):
+            if (sexo == "f" and temp < temp_mas_baja_fem):
                 mombre_temp_mas_baja_fem = nombre
                 temp_mas_baja_fem = temp
 
@@ -123,9 +121,61 @@ class App(customtkinter.CTk):
             cont_while = cont_while + 1
 
 
-                
+        # probemos las variables declaradas previamente
+        # cont_personas = 0 OK
+        # cont_masc = 0
+        # cont_fem = 0
+        # cont_nb = 0
+        # cont_fiebre = 0
+        # cont_menor = 0
+        # cont_menor_y_fiebre = 0
+        # acumu_fiebre = 0
+        # registro_temp_mas_baja_fem = False
+        # mombre_temp_mas_baja_fem = ""
+        # temp_mas_baja_fem = 0
 
-    
+        # verificando la cant de personas
+        print(f"cantidad de: {cont_personas}")
+        # verificando la cant de hombres
+        print(f"cantidad de masc: {cont_masc}")
+        # verificando cant de mujeres
+        print(f"cantidad de fem: {cont_fem}")
+        # vefificando cant de personas nb
+        print(f"cantidad de nb: {cont_nb}")
+        # verificadno cant de personas con fiebre
+        print(f"cantidad personas con fiebre: {cont_fiebre}")
+        # verificando la cant de menores de edad con fiebre
+        print(f"cantidad menores con fiebre: {cont_menor_y_fiebre}")
+        # verificando el acumulador de fiebre
+        print(f"El total de fiebre registrada es de: {acumu_fiebre}")
+        # verificand el nombre y temp de la persona fem con temp mas baja
+        print(f"{mombre_temp_mas_baja_fem} tiene la temp mas baja fem con {temp_mas_baja_fem}")
+
+        # informar el sexo más ingresado
+        if cont_masc > cont_fem and cont_masc > cont_nb:
+            print("El sexo más ingresado fue el masculino")
+        elif cont_fem > cont_masc and cont_fem > cont_nb:
+            print("El sexo más ingresado fue el femenino")
+        elif cont_nb > cont_masc and cont_nb > cont_fem:
+            print("El sexo más ingresado fue el no binario")
+        elif cont_masc == cont_fem and cont_masc > cont_nb:
+            print("Los sexos más ingresados fueron los masculinos y femeninos")
+        elif cont_fem == cont_nb and cont_fem > cont_masc:
+            print("Los sexos más ingresados fueron los femeninos y no binarios")
+        elif cont_masc == cont_nb and cont_masc > cont_fem:
+            print("Los sexos más ingresados fueron los masculinos y no binarios")
+        else:
+            print("Error en los computos")
+
+        # Informar el porcentaje de personas con y sin fiebre
+        porcentaje_con_fiebre = (cont_fiebre / cont_personas) * 100
+        porcentaje_con_fiebre = round(porcentaje_con_fiebre, 2)
+        porcentaje_sin_fiebre = round((100 - porcentaje_con_fiebre), 2)
+        print(f"El % de personas con fiebre es del {porcentaje_con_fiebre}%")
+        print(f"El % de personas sin fiebre es del {porcentaje_sin_fiebre}%")
+
+
+
     
 if __name__ == "__main__":
     app = App()
